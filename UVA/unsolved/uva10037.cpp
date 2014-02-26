86066c0434394a0585740dc23f83e9edf9e0f501
tommy5198 <tommy5198@gmail.com>
2014-02-26 11:34:01 +0800

    uva10037 unsolve

---
+++ UVA/unsolved/uva10037.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+
+
+int main(){
+    int T;
+    scanf("%d", &T);
+    while(T--){
+        int n, sp[1001] = {0};
+
+        scanf("%d", &n);
+        for(int i=0; i<n; i++)
+            scanf("%d", &sp[i]);
+        while(n>3){
+            
+        }
+    }
+}
