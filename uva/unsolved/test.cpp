377b22d5185440e778fd0bf9a0293a080d3d5c72
tommy5198 <tommy5198@gmail.com>
2014-03-05 18:50:09 +0800

    update

--- uva/unsolved/test.cpp
+++ uva/unsolved/test.cpp
@@ -18,18 +18,20 @@ struct ker{
         return a.val < val;  // < is min heap;
     }
 }tmp,now;
-int a[755][755],p[755];
+int a[100][2000],p[100];
 
 int main()
 {
     //freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
 
-    int N,i,j;
-    while(scanf(" %d",&N)==1){
+    int T,N,M,i,j;
+    scanf("%d", &T);
+    while(T--){
+        scanf("%d%d", &M, &N);
         now.val = now.mx = 0;
         now.v.clear();
-        for(i=0;i<N;i++){
+        for(i=0;i<M;i++){
             for(j=0;j<N;j++)
                 scanf(" %d",&a[i][j]);
             sort(a[i],a[i]+N);
@@ -45,12 +47,12 @@ int main()
                 printf("%d",now.val);
             if(n)
                 printf(" ");
-            for(i=0;i<N;i++)
+            for(i=0;i<M;i++)
                 p[i] = 1;
             for(i=0;i<now.v.size();i++)
                 p[now.v[i].first] = now.v[i].second;
 
-            for(i=now.mx;i<N;i++){
+            for(i=now.mx;i<M;i++){
                 tmp = now;
                 tmp.val = tmp.val-a[i][p[i]-1]+a[i][p[i]];
                 tmp.mx = max(tmp.mx, i);
