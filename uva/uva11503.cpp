17c2211ea4a6a69b5d744b04d3d3853c7a960fb7
tommy5198 <tommy5198@gmail.com>
2014-03-12 18:46:48 +0800

    uva11503 AC

--- uva/uva11503.cpp
+++ uva/uva11503.cpp
@@ -10,13 +10,8 @@ int p[200002], r[200002], num[200002];
 
 void init(){
     scanf("%d", &n);
-    k = 1;    
+    k = 1;
     m.clear();
-    for(int i=1; i<200002; i++){
-        p[i] = i;
-        r[i] = 0;
-        num[i] = 1;
-    }
 }
 
 int findset(int x){
@@ -24,7 +19,10 @@ int findset(int x){
 }
 
 void uni(int a, int b){
-    if(a == b) return ;
+    if(a == b){
+        ans = num[a];
+        return ;
+    }
     if(r[a] > r[b]){
         p[b] = a;
         num[a] += num[b];
@@ -41,13 +39,24 @@ void uni(int a, int b){
 int main(){
     int T;
     scanf("%d", &T);
+    //freopen("t.out", "w", stdout);
     while(T--){
         init();
         for(int i=0; i<n; i++){
             string str[2];
             cin>>str[0]>>str[1];
-            if(!m[str[0]]) m[str[0]] = k++;
-            if(!m[str[1]]) m[str[1]] = k++;
+            if(!m[str[0]]){
+                p[k] = k;
+                r[k] = 0;
+                num[k] = 1;
+                m[str[0]] = k++;
+            }
+            if(!m[str[1]]){
+                p[k] = k;
+                r[k] = 0;
+                num[k] = 1;
+                m[str[1]] = k++;
+            }
             uni(findset(m[str[0]]), findset(m[str[1]]));
             printf("%d\n", ans);
         }
