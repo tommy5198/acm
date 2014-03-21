597777bde16004d50c807d1ce44aa4d970cc1f32
tommy5198 <tommy5198@gmail.com>
2014-03-21 23:50:14 +0800

    uva507 AC

--- uva/uva507.cpp
+++ uva/uva507.cpp
@@ -1,3 +1,4 @@
+//DP , FK for 11 WA
 #include<cstdio>
 
 int n;
@@ -11,38 +12,39 @@ void init(){
 
 void sol(int T){
     int len = 0, mx = -1, ml = 0;
-    int tmp, a, b;
+    int tmp, a, ta, tb, b = 0;
+    bool ok = false;
     dp[0] = num[0];
+    if(dp[0] >= 0){
+        len = 1;
+        ta = 0;
+        a = 0;
+        b = 1;
+        ml = 1;
+        mx = dp[0];
+    }
     for(int i=1; i<n-1; i++){
         dp[i] = dp[i-1]>=0 ? dp[i-1]+num[i] : num[i];
-        mx = mx>dp[i] ? mx : dp[i];
-    }
-    if(mx < 0){
-        printf("Route %d has no nice parts", T);
-        return ;
-    }
-    for(int i=n-2; i>=0; i--){
-        if(!len && dp[i] == mx){
-                len = 1;
-                tmp = i+1;
-        }else if(len && dp[i]>=0){
+        if(dp[i]>=0){
+            if(!len)
+                ta = i;
             len++;
-            if(i == 0 && ml <= len){
-                a = 0;
-                b = tmp;
-            }
-        }
-        else if(len && dp[i]<0){
-            if(ml <= len){
-                a = i+1;
-                b = tmp;
-                ml = len;
-            }
+        }else
             len = 0;
+        if(mx < dp[i] || mx == dp[i] && len > ml){
+            b = i+1;
+            a = ta;
+            ml = len;
+            mx = dp[i];
         }
+    }
+    if(mx <= 0){
+        printf("Route %d has no nice parts\n", T);
+        return ;
     }
     printf("The nicest part of route %d is between stops %d and %d\n", T, a+1, b+1);
 }
+
 int main(){
     int T;
     scanf("%d", &T);
